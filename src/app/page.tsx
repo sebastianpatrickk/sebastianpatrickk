46e94c6c0429e1998727bd7efe4becc6d6b9615c
Sebastian Patrick Klen <sebastianpatrickklen@gmail.com>
2024-01-01 15:33:18 +0100

    Refactor layout and header components

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-const Home = () => {
-  return (
-    <div className='font-medium'>
-      <p>Home Page</p>
-    </div>
-  );
-};
-
-export default Home;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,26 +2,26 @@ import {
   GitHubLogoIcon,
   MoonIcon,
   TwitterLogoIcon,
-} from '@radix-ui/react-icons';
-import Link from 'next/link';
-import { Button, buttonVariants } from './ui/button';
-import { ModeToggle } from './ModeToggle';
+} from "@radix-ui/react-icons";
+import Link from "next/link";
+import { Button, buttonVariants } from "./ui/button";
+import { ModeToggle } from "./ModeToggle";
 
 const Header = () => {
   return (
-    <header className='w-full bg-background pt-4'>
-      <div className='container flex h-16 max-w-3xl items-center justify-between'>
-        <Link className='font-bold' href='/'>
-          sebthegg
+    <header className="w-full bg-background pt-4">
+      <div className="container flex h-16 max-w-3xl items-center justify-between">
+        <Link className="font-bold" href="/">
+          sebthepatrick
         </Link>
-        <nav className='flex items-center space-x-1.5 sm:space-x-1'>
-          <Button variant='ghost' size='icon'>
-            <Link href='#'>
+        <nav className="flex items-center space-x-1.5 sm:space-x-1">
+          <Button variant="ghost" size="icon">
+            <Link href="#">
               <GitHubLogoIcon />
             </Link>
           </Button>
-          <Button variant='ghost' size='icon'>
-            <Link href='#'>
+          <Button variant="ghost" size="icon">
+            <Link href="#">
               <TwitterLogoIcon />
             </Link>
           </Button>
